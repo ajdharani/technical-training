2525688e8b1b5435a733df9b6f0cf0b9341e1c57
ajdharani <48052915+ajdharani@users.noreply.github.com>
2019-03-14 19:48:04 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int num,i;
+    printf("enter the number of times:");
+    scanf("%d",&num);
+    for(i=1;i<=num;i++)
+    {
+        printf("hello world..\n");
+    }
+    return 0;
+}
