26f9a559e99bc29d961fdbed7751f544377da540
ajdharani <48052915+ajdharani@users.noreply.github.com>
2019-03-14 19:59:29 +0530

    Create natural.c

---
+++ natural.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int num,i,sum=0;
+    printf("enter any number:");
+    scanf("%d",&num);
+    for(i=1;i<=num;++i)
+    {
+        sum +=i;
+    }
+    printf("sum=  %d",sum);
+    return 0;
+}
+    
