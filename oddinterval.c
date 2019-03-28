2ba94a8f9a95aa84a8b32939721b1402a064ef92
ajdharani <48052915+ajdharani@users.noreply.github.com>
2019-03-28 15:59:26 +0530

    Create oddinterval.c

---
+++ oddinterval.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int m=5,n=10,num;
+    printf("enter the two numbers in a given range m to n:\n");
+    for(num=m;num<n;num++)
+    {
+        if(num%2==1)
+        printf("%d",num);
+    }
+    getch();
+}
