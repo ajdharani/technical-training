a1a9b0bcdb7cfb45f73bdc5c0735f27b88c12591
ajdharani <48052915+ajdharani@users.noreply.github.com>
2019-03-28 16:34:06 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   int n,i;
+   unsigned long long factorial=1;
+   printf("enter the integer");
+   scanf("%d",&n);
+   for(i=1;i<=n;++i)
+   {
+       factorial*i
+   }
+    printf("the factorial number %d=%11u",n.factorial);   
+   }
+
