47aa3584ab708e673dccebd32d53f485679aeba7
ajdharani <48052915+ajdharani@users.noreply.github.com>
2019-08-12 22:26:17 +0530

    Create no of digit.c

---
+++ no of digit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    char string[80];
+    int count, nc = 0, sum = 0;
+ 
+    printf("Enter the string containing both digits and alphabet \n");
+    scanf("%s", string);
+    for (count = 0; string[count] != '\0'; count++)
+    {
+        if ((string[count] >= '0') && (string[count] <= '9'))
+        {
+            nc += 1;
+        }
+    }
+    printf("NO. of Digits in the string = %d\n", nc);
+}
